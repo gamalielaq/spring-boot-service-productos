b113558ccfa1c33792b1fa25595a5b1437c8f915
Gamaliel <gabanto@canvia.com>
2022-10-15 16:50:14 -0500

    Eureka server

--- src/main/java/SpringBoot/app/productos/controllers/ProductoController.java
+++ src/main/java/SpringBoot/app/productos/controllers/ProductoController.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
+// import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +18,8 @@ import SpringBoot.app.productos.models.entity.Producto;
 @RequestMapping("/api")
 public class ProductoController {
 	
-	@Autowired
-	private Environment env;
+	// @Autowired
+	// private Environment env;
 	
 	@Value("${server.port}")
 	private Integer port; 

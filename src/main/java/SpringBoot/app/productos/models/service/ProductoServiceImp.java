e2d86850ef540fa8105e873c9ece0ceb0e405a33
Gamaliel <gabanto@canvia.com>
2022-11-03 21:05:15 -0500

    integración con rest y feing crud completo

--- src/main/java/SpringBoot/app/productos/models/service/ProductoServiceImp.java
+++ src/main/java/SpringBoot/app/productos/models/service/ProductoServiceImp.java
@@ -24,5 +24,17 @@ public class ProductoServiceImp implements IProductoService {
     @Override
     public Producto findById(Long id) {
         return this.productoDao.findById(id).orElse(null);
-    }    
+    }
+
+    @Override
+    @Transactional
+    public Producto save(Producto producto) {
+        return this.productoDao.save(producto);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(Long id) {
+        this.productoDao.deleteById(id);
+    }
 }
